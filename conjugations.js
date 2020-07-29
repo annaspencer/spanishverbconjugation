7385c11dd912d70fb1c2068041cd3a1ad5f81e3f
Anna Spencer <annaspencerdev@gmail.com>
2020-07-29 15:39:19 -0700

    adding delete last btn

--- conjugations.js
+++ conjugations.js
@@ -7,18 +7,15 @@ let nosotrosInput = document.getElementById('nosotros');
 let ellosInput = document.getElementById('ellaUstedesEllos');
 let inputForm = document.getElementById('tableForm');
 let conjugationsTbody = document.querySelector('#conjugationsTable tbody');
-let itemsList =[];
-
 let allData = {};
-let dataId = 0;
-
 let verbs = JSON.parse(localStorage.getItem("verbs")) || [];
+let dataId = verbs.length ? verbs[verbs.length - 1].id : 0;
+
 
 for (let i = 0; i < verbs.length; i++) {
   let newTr = document.createElement('tr');
-    newTr.id = 'data' + dataId;
-    
-  
+    newTr.id = 'data-' + dataId;
+
     appendTd(newTr, verbs[i].verbData);
     appendTd(newTr, verbs[i].transData);
     appendTd(newTr, verbs[i].yoData);
@@ -26,14 +23,18 @@ for (let i = 0; i < verbs.length; i++) {
     appendTd(newTr, verbs[i].elData);
     appendTd(newTr, verbs[i].nosData);
     appendTd(newTr, verbs[i].ellosData);
-    appendDeleteBtn(newTr);
+    appendCheckBtn(newTr);
     conjugationsTbody.append(newTr);
+
+    dataId += 1;
 }
 
 inputForm.addEventListener('submit', submitData);
 
-function submitData(evt){
-    if (evt) evt.preventDefault(); 
+function submitData(evt) {
+    if (evt) {
+      evt.preventDefault();
+    }
 
     let verbData = verbInput.value;
     let transData =transInput.value;
@@ -41,111 +42,104 @@ function submitData(evt){
     let tuData = tuInput.value;
     let elData = elInput.value;
     let nosData = nosotrosInput.value;
-    let ellosData = ellosInput.value; 
-   
+    let ellosData = ellosInput.value;
+
     let curVerb = createVerbData();
     if (curVerb) {
-        dataId += 1;
-    
-        allData['data' + dataId] = curVerb;
-    
+      allData['data-' + dataId] = curVerb;
       appendDataTable(curVerb);
-      
+
       verbInput.value = '';
-      transInput.value='';
+      transInput.value = '';
       yoInput.value = '';
       tuInput.value = '';
       elInput.value = '';
       nosotrosInput.value = '';
       ellosInput.value = '';
-      
-      itemsList.push({ verbData: verbData, transData: transData, yoData: yoData, tuData: tuData, elData: elData, nosData: nosData, ellosData: ellosData});
-    verbs.push({ verbData: verbData, transData: transData, yoData: yoData, tuData: tuData, elData: elData, nosData: nosData, ellosData: ellosData});
+
+      verbs.push({ id: dataId, verbData: verbData, transData: transData, yoData: yoData, tuData: tuData, elData: elData, nosData: nosData, ellosData: ellosData});
       localStorage.setItem("verbs", JSON.stringify(verbs));
-      
+
+      dataId += 1;
   }
 }
 
-  function createVerbData() {
-    let verbData = verbInput.value;
-    let transData =transInput.value;
-    let yoData = yoInput.value;
-    let tuData = tuInput.value;
-    let elData = elInput.value;
-    let nosData = nosotrosInput.value;
-    let ellosData = ellosInput.value; 
+function createVerbData() {
+  let verbData = verbInput.value;
+  let transData =transInput.value;
+  let yoData = yoInput.value;
+  let tuData = tuInput.value;
+  let elData = elInput.value;
+  let nosData = nosotrosInput.value;
+  let ellosData = ellosInput.value;
 
-
-
-    if (verbData === ''){ return;
-  
-     } else {
-      return {
-        verbData: verbData,
-        transData: transData,
-        yoData: yoData,
-        tuData: tuData,
-        elData: elData,
-        nosData: nosData,
-        ellosData: ellosData
-      }
+  if (verbData !== '') {
+    return {
+      verbData: verbData,
+      transData: transData,
+      yoData: yoData,
+      tuData: tuData,
+      elData: elData,
+      nosData: nosData,
+      ellosData: ellosData
     }
-
+  }
 }
-  
 
-  function appendDataTable(curVerb) {
-    let newTr = document.createElement('tr');
-    newTr.id = 'data' + dataId;
-  
-    appendTd(newTr, curVerb.verbData);
-    appendTd(newTr, curVerb.transData);
-    appendTd(newTr, curVerb.yoData);
-    appendTd(newTr, curVerb.tuData);
-    appendTd(newTr, curVerb.elData);
-    appendTd(newTr, curVerb.nosData);
-    appendTd(newTr, curVerb.ellosData);
-    appendDeleteBtn(newTr);
-    conjugationsTbody.append(newTr);
-  
-    
-    
-  }
+function appendDataTable(curVerb) {
+  let newTr = document.createElement('tr');
+  newTr.id = 'data-' + dataId;
 
-  function appendTd(tr, value) {
-    let newTd = document.createElement('td');
-    newTd.innerText = value;
-  
-    tr.append(newTd);
-  }
-  function appendDeleteBtn(tr, type) {
-    let newTd = document.createElement('td');
-    newTd.className = 'deleteBtn';
-    newTd.innerText = 'X';
-  
-    newTd.addEventListener('click', removeEle);
-  
-    tr.append(newTd);
-  }
+  appendTd(newTr, curVerb.verbData);
+  appendTd(newTr, curVerb.transData);
+  appendTd(newTr, curVerb.yoData);
+  appendTd(newTr, curVerb.tuData);
+  appendTd(newTr, curVerb.elData);
+  appendTd(newTr, curVerb.nosData);
+  appendTd(newTr, curVerb.ellosData);
+  appendCheckBtn(newTr);
+  conjugationsTbody.append(newTr);
+}
 
- 
-  function removeEle(evt) {
-    let ele = evt.target.closest('tr');
-    ele.parentNode.removeChild(ele);
-    let index = itemsList.indexOf.call(ele);
-    removeLocalStorage(index)
-    
-  }
+function appendTd(tr, value) {
+  let newTd = document.createElement('td');
+  newTd.innerText = value;
+  tr.append(newTd);
+}
 
-   function removeLocalStorage(index){
-    let store = JSON.parse(localStorage.getItem("verbs")) || [];
-    store.splice(index, 1);
-    localStorage.setItem('verbs', JSON.stringify(store));
-  }
+function appendCheckBtn(tr, type) {
+  let newTd = document.createElement('td');
+  newTd.className = 'checkBtn';
+  newTd.innerHTML = ' <img src="check.png">';
+  newTd.addEventListener('click', removeEle);
+  tr.append(newTd);
+}
 
-  let clear = document.getElementById('clear-button');
-  clear.addEventListener('click', function(){
-    localStorage.clear();
-    window.location.reload(false);
-   
-   })
+
+function removeEle() {
+  let lastRow = conjugationsTbody.lastElementChild
+  let index = verbs.findIndex(item => item.id.toString() === lastRow.id.split('-')[1]);
+  removeLocalStorage(index);
+}
+
+function removeLocalStorage(index){
+  let store = JSON.parse(localStorage.getItem("verbs")) || [];
+  store.splice(index, 1);
+  localStorage.setItem('verbs', JSON.stringify(store));
+}
+
+let deleteLine = document.getElementById('last-button');
+deleteLine.addEventListener('click', function(){
+  removeEle()
+  console.log(conjugationsTbody.lastElementChild);
+  conjugationsTbody.removeChild(conjugationsTbody.lastElementChild);
+})
+
+
+
+let clear = document.getElementById('clear-button');
+
+clear.addEventListener('click', function() {
+  localStorage.clear();
+  window.location.reload(false);
+});
